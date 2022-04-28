d6d9ef48d5bf352b6677d910490cc11417e583ec
unknown <safasherin59@gmail.com>
2022-04-29 01:00:45 +0400

    server modif3

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 3012;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '../public/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
